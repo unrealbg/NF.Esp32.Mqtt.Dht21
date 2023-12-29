e730574a6c8f536ee80219d7eb8874a347b39fb5
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-29 17:04:08 +0200

    feat: Add ConfigManager for centralized config handling

---
+++ ESP32-NF-MQTT-DHT/Configs/ConfigManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace ESP32_NF_MQTT_DHT.Configs
+{
+    public static class ConfigManager
+    {
+        private static Hashtable _settings;
+
+        static ConfigManager()
+        {
+            _settings = new Hashtable();
+            LoadConfig();
+        }
+
+        private static void LoadConfig()
+        {
+            string filePath = "config.txt";
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (var reader = new StreamReader(fileStream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split('=');
+                    if (parts.Length == 2)
+                    {
+                        _settings[parts[0].Trim()] = parts[1].Trim();
+                    }
+                }
+            }
+        }
+
+        public static string GetSetting(string key)
+        {
+            return _settings[key] as string;
+        }
+    }
+}
